57f75ecf9f6663158ff4a75092edec01d8c4e2bb
repoadder <repoadder@gmail.com>
2018-09-12 12:45:51 +0530

    Check any branch scm polling - webhook trigger

--- structurizr-spring/test/unit/com/structurizr/analysis/SpringComponentComponentFinderStrategyTests.java
+++ structurizr-spring/test/unit/com/structurizr/analysis/SpringComponentComponentFinderStrategyTests.java
@@ -31,5 +31,26 @@ public class SpringComponentComponentFinderStrategyTests {
         assertEquals("", component.getDescription());
         assertEquals("Spring Component", component.getTechnology());
     }
+	
+	 @Test
+    public void test_findComponents_FindsTestComponents() throws Exception {
+        Workspace workspace = new Workspace("Name", "Description");
+        Model model = workspace.getModel();
+        SoftwareSystem softwareSystem = model.addSoftwareSystem("Name", "Description");
+        Container container = softwareSystem.addContainer("Name", "Description", "Technology");
+
+        ComponentFinder componentFinder = new ComponentFinder(
+                container,
+                "test.SpringComponentComponentFinderStrategy",
+                new SpringComponentComponentFinderStrategy()
+        );
+        componentFinder.findComponents();
+
+        assertEquals(1, container.getComponents().size());
+        Component component = container.getComponentWithName("SomeComponent");
+        assertEquals("test.SpringComponentComponentFinderStrategy.SomeComponent", component.getType().getType());
+        assertEquals("", component.getDescription());
+        assertEquals("Spring Component", component.getTechnology());
+    }
 
 }
